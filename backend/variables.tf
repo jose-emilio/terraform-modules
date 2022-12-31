b9a77770f5762c806253f6a96183aae059881c5c
José Emilio Vera <joseemilio.vera@iestetuan.es>
2022-12-31 07:19:43 +0000

    v0.2.3

--- backend/variables.tf
+++ backend/variables.tf
@@ -1,8 +1,3 @@
-variable "region" {
-   description = "Región donde se creará el backend (S3 y DynamoDB)"
-   type = string
-}
-
 variable "nombre" {
    description = "Prefijo del bucket de S3 y de la tabla de DynamoDB"
    type = string
